780480e31b7824e2b2cfda19f4797a9064b80442
김민혁 <kimminhyuk@gimminhyeog-ui-MacBookPro.local>
2024-11-03 19:30:55 +0900

    feature: 아티스트 생성 테스트 코드 작성

--- src/main/java/com/bopcon/backend/controller/ArtistApiController.java
+++ src/main/java/com/bopcon/backend/controller/ArtistApiController.java
@@ -1,4 +1,25 @@
 package com.bopcon.backend.controller;
 
+import com.bopcon.backend.domain.Artist;
+import com.bopcon.backend.dto.AddArtistRequest;
+import com.bopcon.backend.service.ArtistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ArtistApiController {
+    private final ArtistService artistService;
+
+    @PostMapping("/api/admin/artist")
+    public ResponseEntity<Artist> addArtist(@RequestBody AddArtistRequest request) {
+        Artist  savedArtist = artistService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArtist);
+    }
+
+
 }
--- src/main/java/com/bopcon/backend/service/ArtistService.java
+++ src/main/java/com/bopcon/backend/service/ArtistService.java
@@ -1,4 +1,16 @@
 package com.bopcon.backend.service;
 
+import com.bopcon.backend.domain.Artist;
+import com.bopcon.backend.dto.AddArtistRequest;
+import com.bopcon.backend.repository.ArtistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class ArtistService {
+    private final ArtistRepository artistRepository;
+
+    // 아티스트 추가 메서드
+    public Artist save(AddArtistRequest request) {  return artistRepository.save(request.toArtist());}
 }
