5437aee838308ecccc4f72b3d300fec92e9201ae
seokjuun <seokjuun@gmail.com>
2024-11-04 17:43:35 +0900

    feat: 아티스트 조회 및 목록 조회

--- src/main/java/com/bopcon/backend/service/ArtistService.java
+++ src/main/java/com/bopcon/backend/service/ArtistService.java
@@ -3,8 +3,11 @@ package com.bopcon.backend.service;
 import com.bopcon.backend.domain.Artist;
 import com.bopcon.backend.dto.AddArtistRequest;
 import com.bopcon.backend.repository.ArtistRepository;
+import com.bopcon.backend.repository.NewConcertRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,4 +16,13 @@ public class ArtistService {
 
     // 아티스트 추가 메서드
     public Artist save(AddArtistRequest request) {  return artistRepository.save(request.toArtist());}
+
+    // 아티스트 목록 가져오기
+    public List<Artist> findAllArtists(){ return artistRepository.findAll(); }
+
+    // 아티스트 조회
+    public Artist findByArtistId(long artistId){
+        return artistRepository.findById(artistId)
+                .orElseThrow(()-> new IllegalArgumentException("not found" + artistId));
+    }
 }
