7ae7e6e21f55bd43eedb7e9b795285f531b17337
seokjuun <seokjuun@gmail.com>
2024-12-04 15:28:01 +0900

    fix: 즐겨찾기 목록 조회 시 아티스트,콘서트 이미지 전달되도록 변경

--- src/main/java/com/bopcon/backend/dto/FavoriteResponse.java
+++ src/main/java/com/bopcon/backend/dto/FavoriteResponse.java
@@ -9,7 +9,9 @@ public class FavoriteResponse {
     private Long favoriteId; // 즐겨찾기 ID
     private Long artistId;   // 아티스트 ID (nullable)
     private String artistName; // 아티스트 이름 (nullable)
+    private String imgUrl;
     private Long newConcertId;  // 콘서트 ID (nullable)
     private String newConcertTitle; // 콘서트 제목 (nullable)
     private String newConcertDate; // 콘서트 날짜 (nullable)
+    private String posterUrl;
 }
